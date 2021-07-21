ff6810eb930794e7aa4b3fc19b35c53f16063d93
lfmcanada <87711256+lfmcanada@users.noreply.github.com>
2021-07-21 08:20:14 -0300

    Update generic_output.py
    fixing from "esponse_type" to "response_type".
    adding space from "selection_policy:str = 'sequential'" to "selection_policy: str = 'sequential'"

--- json_structure/generic_output.py
+++ json_structure/generic_output.py
@@ -5,6 +5,6 @@ from json_structure.output_value import OutputValue
 class GenericOutput:
     values: list[OutputValue] = []
     option : list[Option] = []
-    esponse_type: str = None
-    selection_policy:str = 'sequential'
+    response_type: str = None
+    selection_policy: str = 'sequential'
     repeat_on_reprompt: bool = True
